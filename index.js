9a5b6b17df04e20ea31dfe92e32769b38327d2a8
Jerry <47369872+jerryh-jr@users.noreply.github.com>
2023-01-23 15:28:25 -0500

    add IDs to use for fetch

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const elements = {
     moveList: document.getElementById("movie-list"),
-    url: 'http://www.omdbapi.com/?apikey=[yourkey]&',
-    urlPoster: 'http://img.omdbapi.com/?apikey=[yourkey]&'
+    searchBtnEl: document.getElementById("search__btn--el"),
+    movieInputEl: document.getElementById("movie-input-el"),
+    url: 'http://www.omdbapi.com/?apikey=[apiKey]&',
+    urlPoster: 'http://img.omdbapi.com/?apikey=[apiKey]&'
 }
 
-const res = await fetch(url);
-const movieData = await res.json();
-console.log(movieData);
+function movieFetch() {
+    const { url, urlPoster, movieInputEl } = elements;
+    const res = await fetch(`${url}t=${moveInputEl.value}`);
+    const movieData = await res.json();
+    console.log(movieData);
+};
+
+
+elements.searchBtnEl.addEventListener("click", movieFetch)
