1d081bc8d8ec603a0dd12a91abe5f1bc4fe7f165
Jerry <47369872+jerryh-jr@users.noreply.github.com>
2023-01-26 16:53:18 -0500

    Add function to combine all results from search

--- index.js
+++ index.js
@@ -5,31 +5,47 @@ const elements = {
     movieFormEl: document.getElementById("movie-form"),
     movieListEl: document.getElementById("movie-list"),
     url: 'http://www.omdbapi.com/?apikey=45a430b9&',
-    urlPoster: 'http://img.omdbapi.com/?apikey=45a430b9&'
+    movieArray: [],
 }
 
 async function movieFetch(e) {
     e.preventDefault();
-    const { url, urlPoster, movieInputEl } = elements;
-    const res = await fetch(`${url}t=${movieInputEl.value}`);
+    const { url, movieInputEl } = elements;
+    const res = await fetch(`${url}s=${movieInputEl.value}`);
     const movieData = await res.json();
     console.log(movieData);
     listMovie(movieData);
     debugger;
 }
 
-function listMovie(movieData) {
-    elements.movieListEl.innerHTML = `
-    <h3>${movieData.Title}</h3>
-    <p>${movieData.Actors}</p>
-    <p>${movieData.Metascore}</p>
-    <p>${movieData.Rated}</p>
-    `
+async function getMovieData(movie){
+    const { url, movieArray } = elements;
+    for(let i = 0; i < movieData.Search.length; i++){
+        const res = await fetch(`${url}t=${movie.Search[i]}`);
+        const movies = await res.json();
+        movieArray.push("movies");
+    }
+};
 
-    console.log(movieData.Title)
-    console.log(movieData.Actors)
-    console.log(movieData.Metascore)
-    console.log(movieData.Rated)
+console.log(elements.movieArray)
+function listMovie(movieData) {
+    if(movieData.Error) {
+        return elements.movieListEl.innerHTML = `<h2>Movie Not Found</h2>`;
+    } else {
+        for(let i = 0; i < movieData.Search.length; i++){
+            elements.movieListEl.innerHTML += `
+            <img src="${movieData.Search[i].Poster}" alt="${movieData.Search[i].Title}" width="125" height="175">
+            <h3>${movieData.Search[i].Title}</h3>
+            <p>Actors: ${movieData.Search[i].Actors}</p>
+            <p>Meta Score: ${movieData.Search[i].Metascore}</p>
+            <p>Movie Rating: ${movieData.Search[i].Rated}</p>
+            `
+
+            console.log(movieData.Title)
+            console.log(movieData.Actors)
+            console.log(movieData.Metascore)
+            console.log(movieData.Rated)}
+     }    
 };
 
 window.addEventListener('load', () => {
